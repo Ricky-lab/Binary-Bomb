4017363afd7ea483ec6d3c466df2da40db54b2c2
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2022-02-06 21:43:22 -0500

    Binary Bomb: Using GDB and Vim
    Using GDB on terminal and Vim, compiler the code and debug the program, then decode the password.

---
+++ src/bits_and_bytes.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void print_it() {
+  // TODO
+  printf("TODO\n");
+}
+
+void print_int() {
+  // TODO
+  printf("TODO\n");
+}
+
+void print_float() {
+  // TODO
+  printf("TODO\n");
+}
+
+void print_char() {
+  // TODO
+  printf("TODO\n");
+}
+
+void packing_bytes() {
+  // TODO
+  printf("TODO\n");
+}
+
+void unpacking_bytes() {
+  // TODO
+  printf("TODO\n");
+}
+
+void print_bits() {
+  // TODO
+  printf("TODO\n");
+}
+
+void extracting_fields() {
+  // TODO
+  printf("TODO\n");
+}
+
+void updating_fields() {
+  // TODO
+  printf("TODO\n");
+}
